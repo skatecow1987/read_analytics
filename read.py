2b561a9bcc0dee303912107184fb6f4c8454a759
momo chang <skatecow1987@gmail.com>
2019-04-25 19:25:03 +0800

    calc average

--- read.py
+++ read.py
@@ -7,7 +7,10 @@ with open('reviews.txt', 'r') as f:
 		if count % 5000 == 0: # %用來求餘數 此行表示如果為千的倍數才會印出
 			print(len(data))  #此兩行可以看出讀取進度
 
-print(len(data))
-print(data[0])
-print('--------------')
-print(data[1])
+print('檔案讀取完畢, 總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('留言平均長度是', sum_len / len(data))
+
